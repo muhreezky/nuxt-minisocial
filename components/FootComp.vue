44e01f0e75778751da5b067f381e24414d132063
muhreezky <82578987+muhreezky@users.noreply.github.com>
2023-08-25 06:56:21 +0700

    feat: Page Footer

---
+++ components/FootComp.vue
@@ -0,0 +1,31 @@
+<script>
+export default {
+  name: 'FootComp',
+  data() {
+    return {
+      icons: [
+        "mdi"
+      ]
+    }
+  }
+}
+</script>
+
+<template>
+  <v-footer absolute elevation="3">
+    <v-row>
+      <v-col class="d-flex flex-column align-center justify-center">
+        &copy; 2023 Muhammad Rizkiansyah <br />
+        All Rights Reserved
+      </v-col>
+      <v-col>
+        <b style="margin: auto; text-align: center;">Links</b>
+        <div class="d-flex flex-column">
+          <a href="https://github.com/muhreezky">Github</a>
+          <a href="https://instagram.com/muh.reezky">Instagram</a>
+          <a href="https;//linkedin.com/in/muhreezky">Linkedin</a>
+        </div>
+      </v-col>
+    </v-row>
+  </v-footer>
+</template>
---
+++ components/LoadSpinner.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="container">
+    <div>
+      <v-progress-circular indeterminate color="primary" />
+      <h1>Loading...</h1>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'LoadSpinner'
+}
+</script>
+
+<style scoped>
+.container > div {
+  margin: auto;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 7px;
+}
+.container {
+  min-height: 100vh;
+}
+</style>
