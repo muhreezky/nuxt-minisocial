7b238e77fb0a13c414b668537b2d08fe412a54e7
muhreezky <82578987+muhreezky@users.noreply.github.com>
2023-08-25 08:35:26 +0700

    fix: ditambah link pada footer

--- components/FootComp.vue
+++ components/FootComp.vue
@@ -18,14 +18,21 @@ export default {
         &copy; 2023 Muhammad Rizkiansyah <br />
         All Rights Reserved
       </v-col>
-      <v-col>
-        <b style="margin: auto; text-align: center;">Links</b>
+      <v-col class="text-center">
+        <b class="d-flex items-center justify-center mb-3">Links</b>
         <div class="d-flex flex-column">
           <a href="https://github.com/muhreezky">Github</a>
           <a href="https://instagram.com/muh.reezky">Instagram</a>
-          <a href="https;//linkedin.com/in/muhreezky">Linkedin</a>
+          <a href="https://linkedin.com/in/muhreezky">Linkedin</a>
         </div>
       </v-col>
     </v-row>
   </v-footer>
 </template>
+
+<style>
+  a {
+    text-decoration: none;
+    color: #efefef !important;
+  }
+</style>
