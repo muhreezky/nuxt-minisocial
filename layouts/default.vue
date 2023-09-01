2e5a2528bc118be401485ee01e68762711d510ad
muhreezky <82578987+muhreezky@users.noreply.github.com>
2023-09-01 08:23:44 +0700

    fix: v-for untuk link dasar di navbar

--- layouts/default.vue
+++ layouts/default.vue
@@ -18,8 +18,8 @@
       </v-list-item>
       <v-divider />
       <v-list dense nav>
-        <v-list-item exact nuxt to="/">
-          <v-list-item-title>Home</v-list-item-title>
+        <v-list-item v-for="l in links" :key="l[0]" :to="l[0]" exact nuxt>
+          <v-list-item-title>{{ l[1] }}</v-list-item-title>
         </v-list-item>
         <v-list-item v-for="link in listLinks" :key="link[0]" :to="link[0]" nuxt exact>
           <v-list-item-content>
@@ -28,9 +28,6 @@
         </v-list-item>
         <v-list-item v-if="$auth.loggedIn" href="#" @click.prevent="logout">
           <v-list-item-title>Logout</v-list-item-title>
-        </v-list-item>
-        <v-list-item exact nuxt to="/about">
-          <v-list-item-title>About</v-list-item-title>
         </v-list-item>
       </v-list>
     </v-navigation-drawer>
