9637c030b769e5edfeb0c5cd0fdd0b83980ec750
muhreezky <82578987+muhreezky@users.noreply.github.com>
2023-09-27 16:14:14 +0700

    fix: navlist

--- components/NavList.vue
+++ components/NavList.vue
@@ -1,25 +1,16 @@
 <template>
-	<v-list class="d-flex justify-center align-center" style="width:100%;" rounded>
+	<v-list class="d-flex flex-row justify-center align-center" style="width:100%;" rounded>
 		<v-list-item
 			v-for="link in links"
 			:key="link[0]"
-			:to="link[0]"
-			nuxt
-			exact
+			class="mb-0"
+			@click.prevent="link[0]"
 		>
 			<v-list-item-icon class="mx-auto">
 				<v-icon>{{ link[2] }}</v-icon>
 			</v-list-item-icon>
 			<v-list-item-title :class="display">
 				{{ link[1] }}
-			</v-list-item-title>
-		</v-list-item>
-		<v-list-item v-if="$auth.loggedIn" @click.prevent="logout">
-			<v-list-item-icon class="mx-auto">
-				<v-icon>mdi-logout</v-icon>
-			</v-list-item-icon>
-			<v-list-item-title :class="display">
-				Logout
 			</v-list-item-title>
 		</v-list-item>
 	</v-list>
@@ -31,9 +22,11 @@ export default {
 	data() {
 		return {
 			links: [
-				['/', 'Home', 'mdi-home'],
-				['/about', 'Tentang', 'mdi-information'],
-				['/me', 'My Account', 'mdi-account-box'],
+				[() => this.$router.push('/'), 'Home', 'mdi-home'],
+				[() => this.$router.push('/about'), 'Tentang', 'mdi-information'],
+				[() => null, 'Baru', 'mdi-plus-box'],
+				[() => this.$router.push(`/u/${this.$auth.user.username}`), 'Akun Saya', 'mdi-account-box'],
+				[() => this.logout(), 'Logout', 'mdi-logout']
 			],
 			display: 'd-none d-lg-flex'
 		}
